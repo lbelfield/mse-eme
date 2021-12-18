d5b8e96a50b1dae8a4c352b2955ef3b27f7a1526
Luke Belfield <l.belfield@yahoo.com>
2021-12-18 16:17:55 +0000

    add MSE to boot

--- src/client/boot.jsx
+++ src/client/boot.jsx
@@ -2,12 +2,15 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 
 import Homepage from './homepage';
+import mse from './mse';
 
 const boot = () => {
   if (typeof window !== 'undefined') {
     const wrapper = document.getElementById('container');
     wrapper ? ReactDOM.render(<Homepage />, wrapper) : false;
   }
+
+  mse();
 };
 
 export default boot;
