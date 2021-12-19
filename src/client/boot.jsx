4b1a7b7ff6fe980a7174208e00dbc2d19e52bfde
Luke Belfield <l.belfield@yahoo.com>
2021-12-19 12:19:10 +0000

    OnClick, change the video element's src

--- src/client/boot.jsx
+++ src/client/boot.jsx
@@ -2,17 +2,12 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 
 import Homepage from './components/homepage';
-import mse from './mediaSource/mse';
-import segmentMse from './mediaSource/segmentMse';
 
 const boot = () => {
   if (typeof window !== 'undefined') {
     const wrapper = document.getElementById('container');
     wrapper ? ReactDOM.render(<Homepage />, wrapper) : false;
   }
-
-  // mse();
-  segmentMse();
 };
 
 export default boot;
--- src/client/components/homepage.jsx
+++ src/client/components/homepage.jsx
@@ -2,6 +2,8 @@ import React, {Component} from 'react';
 
 import Form from './form';
 import Video from './video';
+import mse from '../mediaSource/mse';
+import segmentMse from '../mediaSource/segmentMse';
 
 class Homepage extends Component {
   constructor() {
@@ -9,6 +11,7 @@ class Homepage extends Component {
 
     this.state = {
       value: '',
+      videoTitle: 'Click the buttons to choose the video',
     };
   }
 
@@ -16,6 +19,24 @@ class Homepage extends Component {
     this.setState(() => {
       return {
         value: 'Button Clicked',
+      };
+    });
+  }
+
+  handleMseClick() {
+    mse();
+    this.setState(() => {
+      return {
+        videoTitle: 'Bunny - playing...',
+      };
+    });
+  }
+
+  handleSegmentMseClick() {
+    segmentMse();
+    this.setState(() => {
+      return {
+        videoTitle: 'Kuba Workshop - Mango... playing...',
       };
     });
   }
@@ -32,11 +53,21 @@ class Homepage extends Component {
   render() {
     return (
       <div>
+        <h2>{this.state.videoTitle}</h2>
+        <br/>
+        <button onClick={this.handleMseClick.bind(this)}>Bunny</button>
+        <button onClick={this.handleSegmentMseClick.bind(this)}>Kuba Workshop - Mango</button>
+        <br/>
+        <Video width="320" height="240" controls="controls autoplay" />
+        <br/>
+        <br/>
+        <br/>
+        <br/>
+        <br/>
         <h2>{this.state.value}</h2>
         <Form placeholder="video" handleChange={this.handleChange.bind(this)} />
         <button onClick={this.handleClick.bind(this)}>Submit</button>
         <br/>
-        <Video width="320" height="240" controls="controls autoplay" />
       </div>
     );
   }
